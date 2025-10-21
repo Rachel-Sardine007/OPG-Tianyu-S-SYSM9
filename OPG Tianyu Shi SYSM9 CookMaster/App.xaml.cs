5670eafaaf582372b1e6a8bc67de30b1e7f1c33d
Tianyu Shi <tianyushi97@gmail.com>
2025-10-21 16:01:57 +0200

    Projected created while project structure folders and User model added

---
+++ OPG Tianyu Shi SYSM9 CookMaster/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OPG_Tianyu_Shi_SYSM9_CookMaster
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ OPG Tianyu Shi SYSM9 CookMaster/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPG_Tianyu_Shi_SYSM9_CookMaster.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+    }
+}
